8e1417e6e9ef336d9d5f1ab2f9955cf390be3a14
dknight-mazegroup <devin@themazegroup.com>
2023-04-14 16:37:22 -0400

    KSCU-7: Adjust KlaviyoRecreate-Cart controller in SFRA to include correct shipping & tax information when price is recalculated. Organize site imports, beginning clean-up.

--- cartridges/int_klaviyo_sfra/cartridge/controllers/KlaviyoRecreate.js
+++ cartridges/int_klaviyo_sfra/cartridge/controllers/KlaviyoRecreate.js
@@ -1,16 +1,22 @@
 'use strict';
 
 var server = require('server');
-var basketMgr = require('dw/order/BasketMgr');
-var klaviyoUtils = require('*/cartridge/scripts/klaviyo/utils');
-var addedToCartData = require('*/cartridge/scripts/klaviyo/eventData/addedToCart');
-var Transaction = require('dw/system/Transaction');
-var CartModel = require('*/cartridge/models/cart');
-var StringUtils = require('dw/util/StringUtils');
-var cartHelpers = require('*/cartridge/scripts/cart/cartHelpers');
-var PromotionMgr = require('dw/campaign/PromotionMgr');
+
+/* Script Modules */
+var shippingHelper = require('*/cartridge/scripts/checkout/shippingHelpers');
+var COHelpers = require('*/cartridge/scripts/checkout/checkoutHelpers');
 var collections = require('*/cartridge/scripts/util/collections');
+var cartHelpers = require('*/cartridge/scripts/cart/cartHelpers');
+
+/* Models */
+var CartModel = require('*/cartridge/models/cart');
+
+/* API Includes */
+var BasketMgr = require('dw/order/BasketMgr');
 var ProductMgr = require('dw/catalog/ProductMgr');
+var PromotionMgr = require('dw/campaign/PromotionMgr');
+var StringUtils = require('dw/util/StringUtils');
+var Transaction = require('dw/system/Transaction');
 
 
 /**
@@ -25,7 +31,7 @@ var ProductMgr = require('dw/catalog/ProductMgr');
  */
 server.get('Cart', function (req, res, next) {
     var items = JSON.parse(StringUtils.decodeBase64(req.querystring.items));
-    var currentBasket = basketMgr.getCurrentOrNewBasket();
+    var currentBasket = BasketMgr.getCurrentOrNewBasket();
 
     // TODO: Delete following debugging comment regarding basketMgr
     // basketMgr.deleteBasket(currentBasket) // ERROR: user not authorized to act on behalf of customer (NOTE: Only customer service can call this method)
@@ -33,19 +39,17 @@ server.get('Cart', function (req, res, next) {
     // Clean the basket to prevent product duplication on page refresh
     if (currentBasket && currentBasket.productQuantityTotal > 0) {
         for (let i = 0; i < currentBasket.productLineItems.length; i++) {
-            // TODO: Delete following debugging comment about lengthAtIteration variable
-            // Note current iterative length brefore anything is deleted from productLineItems.
             let lengthAtIteration = currentBasket.productLineItems.length;
             var shipmentToRemove = currentBasket.productLineItems[i].shipment;
             currentBasket.removeProductLineItem(currentBasket.productLineItems[i]);
             currentBasket.updateTotals(); // update totals after removing the line item.
             if (currentBasket.productLineItems.length < lengthAtIteration) {
-                // TODO: Delete following debugging comment regarding decrementing index
-                // decrement index by one to account for the productLineItems array shortening by one.
                 i--;
             }
 
-            if (shipmentToRemove.productLineItems.empty && !shipmentToRemove.default) {
+            // TODO: Adjust this...shipping was not being removed from original conditional (issue is shipping is default...)
+            // if (shipmentToRemove.productLineItems.empty && !shipmentToRemove.default) {
+            if (shipmentToRemove.productLineItems.length && !shipmentToRemove.default) {
                 currentBasket.removeShipment(shipmentToRemove);
             }
             PromotionMgr.applyDiscounts(currentBasket);
@@ -53,14 +57,13 @@ server.get('Cart', function (req, res, next) {
     }
 
     // Transaction.begin();
-
     Transaction.wrap(function () {
         if (items && items.length) {
             for (let i = 0; i < items.length; i++) {
                 var currItem = items[i];
                 var productToAdd = ProductMgr.getProduct(items[i].productID);
                 var childProducts = productToAdd.bundledProducts ? collections.map(productToAdd.bundledProducts, function (product) { return { pid: product.ID, quantity: null } }) : []; // TODO: need to identify what quantity would be...
-                var options = []; // TODO: need to identify what this would be...(ex: options from TVs that have warranties? How to include?)
+                var options = [];
                 for (let j = 0; j < currItem.options.length; j++) {
                     options.push({
                         lineItemText: currItem.options[j].lineItemText,
@@ -68,10 +71,12 @@ server.get('Cart', function (req, res, next) {
                         selectedValueId: currItem.options[j].optionValueID
                     })
                 }
-                // TODO: Delete following debugging comment related to addProductToCart call
-                // in here...could call add to cart again for each item....based on query values...
+
+                var shipments = Array.from(currentBasket.shipments);
+                shippingHelper.ensureShipmentHasMethod(shipments[0]);
                 cartHelpers.addProductToCart(currentBasket, items[i].productID, items[i].quantity, childProducts, options)
             }
+            COHelpers.recalculateBasket(currentBasket);
         }
 
         var basketModel = new CartModel(currentBasket);
