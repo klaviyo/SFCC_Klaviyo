5089fa794e08e61195f8f08390388ef3c9bc0614
dknight-mazegroup <devin@themazegroup.com>
2023-04-26 12:37:51 -0400

    KSCU-7: removed unnecessary ATC logic not needed in context of KlaviyoRecreate-Cart (was only needed in original add to cart logic and is instead handled by original core logic after the KlaviyoRecreate-Cart logic has sucessfully rebuilt the cart.

--- cartridges/int_klaviyo/cartridge/scripts/klaviyoATC.js
+++ cartridges/int_klaviyo/cartridge/scripts/klaviyoATC.js
@@ -25,17 +25,10 @@ function addProductToCart(decodedItems, cartObj) {
         var productToAdd;
         var template = 'checkout/cart/minicart';
 
-        if (params.source && params.source.stringValue === 'wishlist' && params.cartAction && params.cartAction.stringValue === 'update') {
-            app.getController('Wishlist').ReplaceProductListItem();
-            return;
-        } else {
-            var previousBonusDiscountLineItems = cart.getBonusDiscountLineItems();
-            for (let i = 0; i < productList.length; i++) {
-                productToAdd = Product.get(productList[i].productID);
-                productOptionModel = productToAdd ? recreateHelpers.updateOptions(productList[i], productToAdd.object) : null;
-                cart.addProductItem(productToAdd.object, productList[i].quantity, productOptionModel);
-                newBonusDiscountLineItem = cart.getNewBonusDiscountLineItem(previousBonusDiscountLineItems);
-            }
+        for (let i = 0; i < productList.length; i++) {
+            productToAdd = Product.get(productList[i].productID);
+            productOptionModel = productToAdd ? recreateHelpers.updateOptions(productList[i], productToAdd.object) : null;
+            cart.addProductItem(productToAdd.object, productList[i].quantity, productOptionModel);
         }
 
         return {
