77c15d0f9c6cf70975ada5fb8fbd366605f088be
Aidrian O'Connor <public@unitone.org>
2023-05-23 10:35:11 -0400

    KSCU-74: WIP check-in, verbose commenting continued

--- _sitegen/controllers/COCustomer.js
+++ _sitegen/controllers/COCustomer.js
@@ -38,13 +38,14 @@ function start() {
 
 
     /***
-     * KL EVENT TRACKING: Started Checkout event
+     * KL EVENT TRACKING: Started Checkout event, triggered via appending the OOTB Checkout-Begin controller
      * Utilizes startedCheckout.js > getData() via checkoutHelpers.js > startedCheckoutHelper(...) to
      *  assemble event data and utils.js > trackEvent(...) to transmit it to the KL API
      * Note that checkoutHelpers.js > getEmailFromBasket() is used to extract the order email from the current basket
      *  (if available).  Started Checkout events use customer email and not KL exchangeID for identifying the user.
-     * Also note that no client side debugging is possible for this event as SFCC won't accept additional QS parameters
-     *  in checkout controllers.  Instead rely on server side logs to debug Started Checkout events.
+     * Also note that client side debugging may or may not be possible for this event depending on site-specific customizations
+     *  of checkoout flow.  Refer to documentation on the Started Checkout event for more information, and if nothing else
+     *  rely on server side logs to debug Started Checkout events.
     ***/
     var KLCheckoutHelpers = require('*/cartridge/scripts/klaviyo/checkoutHelpers');
     var customerEmail = KLCheckoutHelpers.getEmailFromBasket();
