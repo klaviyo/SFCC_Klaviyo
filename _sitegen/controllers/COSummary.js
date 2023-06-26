606ace2d5201ed5deaef95f65cc5d8135fa9984b
Aidrian O'Connor <public@unitone.org>
2023-06-26 18:42:58 -0400

    KSCU-78: adding checkboxes for users to subscribe to lists - sitegen files and metadeta.zip update

--- _sitegen/controllers/COSummary.js
+++ _sitegen/controllers/COSummary.js
@@ -103,6 +103,23 @@ function showConfirmation(order) {
                 dataObj = orderConfirmationData.getData(order, exchangeID);
                 serviceCallResult = klaviyoUtils.trackEvent(exchangeID, dataObj, klaviyoUtils.EVENT_NAMES.orderConfirmation, order.customerEmail);
             }
+
+            if('KLEmailSubscribe' in session.custom || 'KLSmsSubscribe' in session.custom) {
+                var email = session.custom.KLEmailSubscribe ? order.customerEmail : false;
+                var phone = session.custom.KLSmsSubscribe ? order.defaultShipment.shippingAddress.phone : false;
+                var e164PhoneRegex = new RegExp(/^\+[1-9]\d{1,14}$/);
+                if(phone) {
+                    // NOTE: Klaviyo only accepts phone numbers that include + and the country code at the start (ex for US: +16465551212)
+                    // in order to successfully get users subscribed to SMS list you must collect the country code in your order phone number field!
+                    phone = '+' + phone.replace(/[^a-z0-9]/gi, '');
+                    if(!e164PhoneRegex.test(phone)) {
+                        phone = false;
+                    }
+                }
+                if(email || phone) {
+                    klaviyoUtils.subscribeUser(email, phone);
+                }
+            }
         }
     }
     /* END Klaviyo Order Confirmation event tracking */
