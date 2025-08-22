30a7bbb64b7376e672f887db8d86017da09a3e4b
Claire Kolln <78871498+cykolln@users.noreply.github.com>
2025-08-22 12:05:36 -0400

    Version bump for 25.8.0 (#53)
    Release 25.8.0

--- cartridges/int_klaviyo_core/cartridge/scripts/klaviyo/services.js
+++ cartridges/int_klaviyo_core/cartridge/scripts/klaviyo/services.js
@@ -18,7 +18,7 @@ function getApiVersion() {
  * @returns {string} User-Agent string
  */
 function getUserAgent() {
-    return 'sfcc-klaviyo/25.7.0';
+    return 'sfcc-klaviyo/25.8.0';
 }
 
 // HTTP Services
